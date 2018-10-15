bea75b419e73843ac0c12fa14c4e1fa584846f44
akels <akels14@gmail.com>
2018-10-15 20:47:01 +0200

    Fixing improvemeshcol

--- test/test.jl
+++ test/test.jl
@@ -1,6 +1,33 @@
 using ElTopo
 using JLD
-
 @load "test/sphere.jld"
+
+@info "Testing improvemesh"
+
 p = Elparameters()
 improvemesh(points,faces,p)
+
+@info "Testing improvemeshcol"
+
+function velocity(t,pos)
+    x,y,z = pos
+    
+    x = x*0.15 + 0.35
+    y = y*0.15 + 0.35
+    z = z*0.15 + 0.35
+
+    u = 2*sin(pi*x)^2 * sin(2*pi*y) * sin(2*pi*z) * sin(2/3*pi*t)
+    v = - sin(2*pi*x) * sin(pi*y)^2 * sin(2*pi*z) * sin(2/3*pi*t)
+    w = - sin(2*pi*x) * sin(2*pi*y) * sin(pi*z)^2 * sin(2/3*pi*t)
+
+    [u,v,w] /0.15
+end
+
+v = zero(points)
+t = 1
+for i in 1:size(points,2)
+    v[:,i] = velocity(t,points[:,i])
+end
+
+par = Elparameters(m_dt=1.0)
+improvemeshcol(points,faces,points + 0.01*v,par)
